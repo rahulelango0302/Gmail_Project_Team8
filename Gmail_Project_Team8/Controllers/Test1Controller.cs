45e8bf1cafdfdc41cda057227c95c876cf1467f9
rahulelango0302 <rahulelango1999@gmail.com>
2022-06-29 13:23:58 +0530

    New controller Test1 created and login1 view page created

---
+++ Gmail_Project_Team8/Controllers/Test1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gmail_Project_Team8.Controllers
+{
+    public class Test1Controller : Controller
+    {
+        // GET: Test1
+        public ActionResult Login1()
+        {
+            return View();
+        }
+    }
+}
