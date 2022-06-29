ba4fd45c22773e1301cf70482f4fdd5dc1624a00
Raghul E <1000071060@hexaware.com>
2022-06-29 12:51:37 +0530

    Services created

---
+++ Gmail_Project_Team8/Controllers/ServicesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gmail_Project_Team8.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
