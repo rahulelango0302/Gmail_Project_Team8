3dd649ce73a59c23e7cd54161bc360237a313ac9
Raghul E <1000071060@hexaware.com>
2022-06-29 12:24:31 +0530

    Created a model - Login

---
+++ Gmail_Project_Team8/Models/AccountModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gmail_Project_Team8.Models
+{
+    public class Login
+    {
+        public string UserName { get; set; }
+        public string password { get; set; }
+    }
+}
