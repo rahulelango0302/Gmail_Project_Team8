a4bf5dcf15e65e79ead5da3f8eea924a2a6c905a
1000071060 <1000071060@Maverick-VM-01>
2022-06-29 12:13:45 +0530

    Add project files.

---
+++ Gmail_Project_Team8/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Gmail_Project_Team8
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Gmail_Project_Team8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Gmail_Project_Team8.MvcApplication" Language="C#" %>
